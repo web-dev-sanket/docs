de364f5275b06c87b3e66a69f72ec9b93b61376b
web-dev-sanket <web.sanketkamble@gmail.com>
2024-11-10 16:13:56 +0530

    documents

---
+++ src/components/Foreground.jsx
@@ -0,0 +1,47 @@
+import React, { useRef } from "react";
+import Card from "./Card";
+
+function Foreground() {
+  const ref = useRef(null);
+
+  const data = [
+    {
+      desc: " Lorem ipsum dolor sit amet consectetur adipisicing.",
+      filesize: "0.9mb",
+      close: true,
+      tag: { isOpen: true, tagTitle: "Download Now", tagColor: "green" },
+    },
+    {
+      desc: " Lorem ipsum dolor sit amet consectetur adipisicing.",
+      filesize: "0.9mb",
+      close: true,
+      tag: { isOpen: true, tagTitle: "Download Now", tagColor: "blue" },
+    },
+    {
+      desc: " Lorem ipsum dolor sit amet consectetur adipisicing.",
+      filesize: "0.9mb",
+      close: true,
+      tag: { isOpen: true, tagTitle: "Download Now", tagColor: "green" },
+    },
+    {
+      desc: " Lorem ipsum dolor sit amet consectetur adipisicing.",
+      filesize: "0.9mb",
+      close: true,
+      tag: { isOpen: true, tagTitle: "Download Now", tagColor: "blue" },
+    },
+  ];
+  return (
+    <>
+      <div
+        ref={ref}
+        className="fixed top-0 left-0 z-[3] w-full h-full flex gap-10 flex-wrap p-5 "
+      >
+        {data.map((item, index) => (
+          <Card data={item} reference={ref} />
+        ))}
+      </div>
+    </>
+  );
+}
+
+export default Foreground;
